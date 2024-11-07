4d61d2ad58eecc4537dae4ce96e2bd1f0d7bd531
PurpleMyst <PurpleMyst@users.noreply.github.com>
2024-11-07 16:36:28 +0100

    feat: add benchmarks

---
+++ benchmark/benches/benchmarks.rs
@@ -0,0 +1,57 @@
+use criterion::{{criterion_group, criterion_main, Criterion}};
+use quest01::solve_part;
+use quest02::{{solve_part1, solve_part2, solve_part3}};
+use quest03::solve;
+
+fn bench_all(c: &mut Criterion) {{
+    // Benchmarks for quest01
+    let input1_q1 = include_str!("../../quest01/src/part1.txt");
+    c.bench_function("quest01_part1", |b| {{
+        b.iter(|| solve_part::<1>(input1_q1))
+    }});
+
+    let input2_q1 = include_str!("../../quest01/src/part2.txt");
+    c.bench_function("quest01_part2", |b| {{
+        b.iter(|| solve_part::<2>(input2_q1))
+    }});
+
+    let input3_q1 = include_str!("../../quest01/src/part3.txt");
+    c.bench_function("quest01_part3", |b| {{
+        b.iter(|| solve_part::<3>(input3_q1))
+    }});
+
+    // Benchmarks for quest02
+    let input1_q2 = include_str!("../../quest02/src/part1.txt");
+    c.bench_function("quest02_part1", |b| {{
+        b.iter(|| solve_part1(input1_q2))
+    }});
+
+    let input2_q2 = include_str!("../../quest02/src/part2.txt");
+    c.bench_function("quest02_part2", |b| {{
+        b.iter(|| solve_part2(input2_q2))
+    }});
+
+    let input3_q2 = include_str!("../../quest02/src/part3.txt");
+    c.bench_function("quest02_part3", |b| {{
+        b.iter(|| solve_part3(input3_q2))
+    }});
+
+    // Benchmarks for quest03
+    let input1_q3 = include_str!("../../quest03/src/part1.txt");
+    c.bench_function("quest03_part1", |b| {{
+        b.iter(|| solve(input1_q3, false))
+    }});
+
+    let input2_q3 = include_str!("../../quest03/src/part2.txt");
+    c.bench_function("quest03_part2", |b| {{
+        b.iter(|| solve(input2_q3, false))
+    }});
+
+    let input3_q3 = include_str!("../../quest03/src/part3.txt");
+    c.bench_function("quest03_part3", |b| {{
+        b.iter(|| solve(input3_q3, true))
+    }});
+}}
+
+criterion_group!(benches, bench_all);
+criterion_main!(benches);
