16f497decd25daa418bb26a4ff8e94bc90283c90
PurpleMyst <PurpleMyst@users.noreply.github.com>
2024-11-10 18:29:30 +0100

    perf(quest03): use grid crate

--- quest03/src/lib.rs
+++ quest03/src/lib.rs
@@ -1,69 +1,66 @@
-use std::mem::swap;
+use grid::Grid;
 
 #[inline]
 pub fn solve(input: &str, include_diagonals: bool) -> u16 {
-    let mut depthmap: Vec<Vec<u16>> = Vec::new();
-    let mut diggable: Vec<Vec<bool>> = Vec::new();
+    let mut depthmap: Grid<u16> = Grid::new(0, 0);
+    let mut digmap: Grid<bool> = Grid::new(0, 0);
 
     for row in input.lines() {
-        diggable.push(row.bytes().map(|b| b == b'#').collect());
-        depthmap.push(row.bytes().map(|_| 0).collect());
+        digmap.push_row(row.bytes().map(|b| b == b'#').collect());
+        depthmap.push_row(row.bytes().map(|_| 0).collect());
     }
 
-    let height = depthmap.len();
-    let width = depthmap[0].len();
-    let mut next_depthmap = Vec::new();
+    let height = depthmap.rows();
+    let width = depthmap.cols();
+    let mut next_depthmap = depthmap.clone();
 
     loop {
         let mut dug = false;
 
-        next_depthmap.clone_from(&depthmap);
-        for (y, row) in depthmap.iter().enumerate() {
-            for (x, &current_depth) in row.iter().enumerate() {
-                if !diggable[y][x] {
-                    continue;
-                }
-
-                let can_dig = if include_diagonals {
-                    [
-                        (x != 0).then(|| depthmap[y][x - 1]),
-                        (x != width - 1).then(|| depthmap[y][x + 1]),
-                        (y != 0).then(|| depthmap[y - 1][x]),
-                        (y != height - 1).then(|| depthmap[y + 1][x]),
-                        (x != 0 && y != 0).then(|| depthmap[y - 1][x - 1]),
-                        (x != width - 1 && y != 0).then(|| depthmap[y - 1][x + 1]),
-                        (x != 0 && y != height - 1).then(|| depthmap[y + 1][x - 1]),
-                        (x != width - 1 && y != height - 1).then(|| depthmap[y + 1][x + 1]),
-                    ]
-                    .map(|n| n.unwrap_or(0))
-                    .into_iter()
-                    .all(|h| h == current_depth)
-                } else {
-                    [
-                        (x != 0).then(|| depthmap[y][x - 1]),
-                        (x != width - 1).then(|| depthmap[y][x + 1]),
-                        (y != 0).then(|| depthmap[y - 1][x]),
-                        (y != height - 1).then(|| depthmap[y + 1][x]),
-                    ]
-                    .into_iter()
-                    .flatten()
-                    .all(|h| h == current_depth)
-                };
-
-                if can_dig {
-                    next_depthmap[y][x] = current_depth + 1;
-                    dug = true;
-                }
-
-                diggable[y][x] = can_dig;
+        for (((y, x), &depth), diggable) in depthmap.indexed_iter().zip(digmap.iter_mut()) {
+            if !*diggable {
+                continue;
             }
+
+            let can_dig = if include_diagonals {
+                [
+                    (x != 0).then(|| depthmap[(y, x - 1)]),
+                    (x != width - 1).then(|| depthmap[(y, x + 1)]),
+                    (y != 0).then(|| depthmap[(y - 1, x)]),
+                    (y != height - 1).then(|| depthmap[(y + 1, x)]),
+                    (x != 0 && y != 0).then(|| depthmap[(y - 1, x - 1)]),
+                    (x != width - 1 && y != 0).then(|| depthmap[(y - 1, x + 1)]),
+                    (x != 0 && y != height - 1).then(|| depthmap[(y + 1, x - 1)]),
+                    (x != width - 1 && y != height - 1).then(|| depthmap[(y + 1, x + 1)]),
+                ]
+                .map(|n| n.unwrap_or(0))
+                .into_iter()
+                .all(|h| h == depth)
+            } else {
+                [
+                    (x != 0).then(|| depthmap[(y, x - 1)]),
+                    (x != width - 1).then(|| depthmap[(y, x + 1)]),
+                    (y != 0).then(|| depthmap[(y - 1, x)]),
+                    (y != height - 1).then(|| depthmap[(y + 1, x)]),
+                ]
+                .into_iter()
+                .flatten()
+                .all(|h| h == depth)
+            };
+
+            if can_dig {
+                next_depthmap[(y, x)] = depth + 1;
+                dug = true;
+            }
+
+            *diggable = can_dig;
         }
-        swap(&mut depthmap, &mut next_depthmap);
+        depthmap.clone_from(&next_depthmap);
 
         if !dug {
             break;
         }
     }
 
-    depthmap.into_iter().flatten().sum()
+    depthmap.into_vec().into_iter().sum()
 }
